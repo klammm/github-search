b4110d15e43db788822eefe605a19a0ceec7b8c7
Kevin Lam <kevinlam.sf@gmail.com>
2022-02-18 12:40:23 -0800

    final commit for page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,67 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React, { useEffect, useState } from "react";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import RepoSearch from "../components/repo-search";
+import SearchResults from "../components/search-results";
+import Pagination from "../components/pagination";
+import { getGithubData } from "../utils/api";
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState({ items: [] });
+  const [error, setError] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    getGithubData({
+      currentPage,
+      setSearchResults,
+      setError,
+      searchQuery,
+    })
+  };
+
+  useEffect(() => {
+    if (searchQuery) {
+      getGithubData({
+        currentPage,
+        setSearchResults,
+        setError,
+        searchQuery,
+      })
+    }
+  }, [currentPage])
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold">Start your search now!</h1>
+      <RepoSearch
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        onSubmit={handleSubmit}
+      />
+      {searchResults.items.length !== 0 && (
+        <>
+          <h1>{searchResults.total_count} repositories found</h1>
+          <SearchResults results={searchResults.items} />
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            searchQuery={searchQuery}
+            setSearchResults={setSearchResults}
+            setError={setError}
+            length={searchResults.total_count}
+          />
+        </>
+      )}
+    </Layout>
+  )
+};
 
 export default IndexPage
