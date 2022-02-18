a6ebd0c57e35fb60a1494ddce92774a58e5ecdf9
Kevin Lam <kevinlam.sf@gmail.com>
2022-02-18 12:32:22 -0800

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gsw-blue": "#1D428A",
+        "gsw-gold": "#FFC72C",
+      },
+    },
+  },
+  plugins: [],
+}
