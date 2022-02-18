4984566ba7dc909d763654f59be12d33f99fe5bc
Kevin Lam <kevinlam.sf@gmail.com>
2022-02-18 12:33:07 -0800

    add optional chaining

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Github Search`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-optional-chaining`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
